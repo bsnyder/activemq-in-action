00278fccd02593e93767e388469f2ba598936cea
dejan@nighttale.net <dejan@nighttale.net@a7836bfc-094a-0410-be50-4d7e4d37fb6f>
2008-04-11 14:07:24 +0000

    chapter 5 code samples

---
+++ src/main/java/org/apache/activemq/book/ch2/jobs/readme.txt
@@ -0,0 +1,3 @@
+1. Start ActiveMQ: ${ACTIVEMQ_HOME}/bin/activemq xbean:src/main/resources/org/apache/activemq/book/ch2/activemq.xml
+2. Run Publisher class
+3. Run Consumer class
---
+++ src/main/java/org/apache/activemq/book/ch5/AuthorizationPolicy.java
@@ -0,0 +1,23 @@
+package org.apache.activemq.book.ch5;
+
+import org.apache.activemq.broker.ConnectionContext;
+import org.apache.activemq.command.Message;
+import org.apache.activemq.security.MessageAuthorizationPolicy;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class AuthorizationPolicy implements
+		MessageAuthorizationPolicy {
+	
+    private static final Log LOG = LogFactory.getLog(AuthorizationPolicy.class);
+
+	public boolean isAllowedToConsume(ConnectionContext context, Message message) {
+		LOG.info(context.getConnection().getRemoteAddress());
+		if (context.getConnection().getRemoteAddress().startsWith("/127.0.0.1")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
