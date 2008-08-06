78b957d554950879b4624d548d7f3c7aba1b05d7
dejan@nighttale.net <dejan@nighttale.net@a7836bfc-094a-0410-be50-4d7e4d37fb6f>
2008-08-06 13:01:10 +0000

    modified example

--- src/main/java/org/apache/activemq/book/ch6/spring/SpringBroker.java
+++ src/main/java/org/apache/activemq/book/ch6/spring/SpringBroker.java
@@ -1,20 +1,26 @@
 package org.apache.activemq.book.ch6.spring;
 
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.apache.activemq.book.ch5.Publisher;
+import org.apache.xbean.spring.context.FileSystemXmlApplicationContext;
 
 
 public class SpringBroker {
 
-	//Spring 1.0
-	static String broker = "src/main/resources/org/apache/activemq/book/ch7/spring-1.0.xml";
-	//Spring + XBean
-	//static String broker = "src/main/resources/org/apache/activemq/book/ch5/activemq-simple.xml";
-	//Spring 2.0
-	//static String broker = "src/main/resources/org/apache/activemq/book/ch7/spring-2.0.xml";
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+    	if (args.length == 0) {
+    		System.err.println("Please define a configuration file!");
+    		return;
+    	}
+    	String brokerUrl = args[0];
+    	System.out.println("Starting broker with the following configuration: " + brokerUrl);
 		System.setProperty("activemq.base", System.getProperty("user.dir"));
-		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext(broker);
+		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext(brokerUrl);
+		
+		Publisher publisher = new Publisher();
+		for (int i = 0; i < 100; i++) {
+			publisher.sendMessage(new String[]{"JAVA", "IONA"});
+		}
+		
 	}
 
 }
