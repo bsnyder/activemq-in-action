f749dfe1edeed20f7f7384b0757c8fec0f27a366
dejan@nighttale.net <dejan@nighttale.net@a7836bfc-094a-0410-be50-4d7e4d37fb6f>
2008-08-21 12:06:05 +0000

    configuation file moved to ch6 package

--- src/main/java/org/apache/activemq/book/ch6/spring/SpringClient.java
+++ src/main/java/org/apache/activemq/book/ch6/spring/SpringClient.java
@@ -1,11 +1,14 @@
 package org.apache.activemq.book.ch6.spring;
 
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.apache.xbean.spring.context.FileSystemXmlApplicationContext;
+
 
 public class SpringClient {
 
 	public static void main(String[] args) {
-		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/org/apache/activemq/book/ch7/spring-client.xml");
+		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/org/apache/activemq/book/ch6/spring-client.xml");
+		SpringPublisher publisher = (SpringPublisher)context.getBean("stockPublisher");
+		publisher.start();
 	}
 
 }
